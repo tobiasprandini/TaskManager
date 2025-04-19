465424d11828fe450046336171121b3443fc6d61
Tobias Prandini <tobiasprandini@gmail.com>
2025-04-19 14:48:31 -0300

    chore: add tailwind prettier plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-    return (
-        <div>
-            <h1 className="text-red-500 p-6">Task Manager</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="mt-2 bg-red-500 p-6">Task Manager</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
