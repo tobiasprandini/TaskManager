c50307c7536954a85d6033a958e005a92bbe7fc3
Tobias Prandini <tobiasprandini@gmail.com>
2025-04-23 08:08:14 -0300

    feat: ensure tasks are listed on Tasks component

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,24 @@
+const TaskItem = ({ task }) => {
+  const getStatusClasses = () => {
+    if (task.status === "done") {
+      return "bg-[#00ADB5] bg-opacity-10 text-[#00ADB5]"
+    }
+
+    if (task.status === "in_progress") {
+      return "bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]"
+    }
+
+    if (task.status === "not_started") {
+        return "bg-[#2B2D42] bg-opacity-10 text-[#2B2D42]"
+    }
+  }
+  return (
+    <div
+      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+    >
+      {task.title}
+    </div>
+  )
+}
+
+export default TaskItem
