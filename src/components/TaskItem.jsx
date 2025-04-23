2c6f95972465246ab3ac069a0996a51f2c69a80a
Tobias Prandini <tobiasprandini@gmail.com>
2025-04-23 08:37:40 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,22 +1,44 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import LoaderIcon from "../assets/icons/loader.svg?react"
+import DetailsIcon from "../assets/icons/details.svg?react"
+
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === "done") {
-      return "bg-[#00ADB5] bg-opacity-10 text-[#00ADB5]"
+      return "bg-[#00ADB5] text-[#00ADB5]"
     }
 
     if (task.status === "in_progress") {
-      return "bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]"
+      return "bg-[#FFAA04] text-[#FFAA04]"
     }
 
     if (task.status === "not_started") {
-        return "bg-[#2B2D42] bg-opacity-10 text-[#2B2D42]"
+      return "bg-[#2B2D42] bg-opacity-10 text-[#2B2D42]"
     }
   }
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && <LoaderIcon />}
+        </label>
+
+        {task.title}
+      </div>
+
+      <a href="#" className="transition hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   )
 }
