41012d07c9dadcb426ece4ed59ab7a5739482d0a
Tobias Prandini <tobiasprandini@gmail.com>
2025-04-26 14:57:30 -0300

    style: add sort import plugin

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,4 @@
-import { CheckIcon, LoaderIcon, DetailsIcon, TrashIcon } from "../assets/icons"
+import { CheckIcon, DetailsIcon, LoaderIcon, TrashIcon } from "../assets/icons"
 import Button from "./Button"
 
 const TaskItem = ({ task, handleCheckboxClick, handleDeleteClick }) => {
